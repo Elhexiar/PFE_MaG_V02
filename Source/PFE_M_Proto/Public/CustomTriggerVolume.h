826821ba5e37d6a77ccc38e2a47f40839f33c8da
MIRIEL Mathis <118454205+Elhexiar@users.noreply.github.com>
2024-12-03 16:46:50 +0100

    adding CustomTriggerBox and part1 of its logic

---
+++ Source/PFE_M_Proto/Public/CustomTriggerVolume.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerVolume.h"
+#include "CustomTriggerVolume.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class PFE_M_PROTO_API ACustomTriggerVolume : public ATriggerVolume
+{
+	GENERATED_BODY()
+	
+};
